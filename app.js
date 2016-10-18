056889faf9530769878695ca427fe1d042d5e4d0
avengang <544160071@qq.com>
2016-10-19 00:43:19 +0800

    阿斯蒂芬

--- app.js
+++ app.js
@@ -1,54 +1,122 @@
-var http = require("http");
-var io= require("socket.io"); 
-var server = http.createServer(function(request, response){
-    response.writeHead(200,{"Content-Type":"text/html"});
-    response.write("WebSocket Start~~~~~~~~~~~~");
-    response.end("");
-}).listen(80);
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-var socket= io.listen(server); 
+router = require('./routes/index');
+require('./routes/hey');
 
-// 添加一个连接监听器
-socket.on("connection", function(client){ 
+var app = express();
 
-  client.on("message",function(event){ 
-    console.log("Received message from client!",event); 
-    client.emit("emitMessage", { hello: "messgge received, wish you happy"+new Date().toString() });
-    db();
-    client.emit("emitMessage", { hello: "messgge received1, wish you happy"+new Date().toString() });
-  }); 
-  client.on("disconnect",function(){ 
-   // clearInterval(interval); 
-    console.log("Server has disconnected"); 
-  }); 
-  client.send("hello, I am the server");
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+var swig = require('swig');
+app.set('view engine', 'html');
+app.engine('html', swig.renderFile);
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', router);
+//app.use('/hey', hey);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 
-function db() {
-	var MongoClient = require('mongodb').MongoClient;
-	var DB_CONN_STR = 'mongodb://192.168.1.19:27017/d5459f61-def0-4735-8906-ad263a055ee2';    
+module.exports = app;
+
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+//在线用户
+var onlineUsers = {};
+//当前在线人数
+var onlineCount = 0;
+
+io.on('connection', function(socket){
+	console.log('a user connected');
 	
-	var insertData = function(db, callback) {  
-	    //连接到表  
-	    var collection = db.collection('tb2');
-	    //插入数据
-	    var data = [{"name":'wilson001',"age":21},{"name":'wilson002',"age":22}];
-	    collection.insert(data, function(err, result) { 
-	        if(err)
-	        {
-	            console.log('Error:'+ err);
-	            return;
-	        }     
-	        callback(result);
-	    });
-	}
-	
-	MongoClient.connect(DB_CONN_STR, function(err, db) {
-	    console.log("连接成功！");
-	    insertData(db, function(result) {
-	        console.log(result);
-	        db.close();
-	    });
+	//监听新用户加入
+	socket.on('login', function(obj){
+		//将新加入用户的唯一标识当作socket的名称，后面退出的时候会用到
+		socket.name = obj.userid;
+		
+		//检查在线列表，如果不在里面就加入
+		if(!onlineUsers.hasOwnProperty(obj.userid)) {
+			onlineUsers[obj.userid] = obj.username;
+			//在线人数+1
+			onlineCount++;
+		}
+		
+		//向所有客户端广播用户加入
+		io.emit('login', {onlineUsers:onlineUsers, onlineCount:onlineCount, user:obj});
+		console.log(obj.username+'加入了聊天室');
 	});
-}
+	
+	//监听用户退出
+	socket.on('disconnect', function(){
+		//将退出的用户从在线列表中删除
+		if(onlineUsers.hasOwnProperty(socket.name)) {
+			//退出用户的信息
+			var obj = {userid:socket.name, username:onlineUsers[socket.name]};
+			
+			//删除
+			delete onlineUsers[socket.name];
+			//在线人数-1
+			onlineCount--;
+			
+			//向所有客户端广播用户退出
+			io.emit('logout', {onlineUsers:onlineUsers, onlineCount:onlineCount, user:obj});
+			console.log(obj.username+'退出了聊天室');
+		}
+	});
+	
+	//监听用户发布聊天内容
+	socket.on('message', function(obj){
+		//向所有客户端广播发布的消息
+		io.emit('message', obj);
+		console.log(obj.username+'说：'+obj.content);
+	});
+  
+});
+
+http.listen(80, function(){
+	console.log('listening on *:80');
+});
